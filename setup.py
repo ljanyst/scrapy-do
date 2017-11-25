c5f58c345504573b3f11363e56234d17d230d7d9
Lukasz Janyst <lukasz@jany.st>
2017-11-25 15:30:40 +0100

    dist: Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name = 'scrapy-do',
+    version = '0.1',
+    author = 'Lukasz Janyst',
+    author_email = 'xyz@jany.st',
+    url = 'https://github.com/ljanyst/scrapy-do',
+    description = 'Spider Runner for Scrapy',
+    long_description = long_description,
+    license = 'BSD License',
+    packages = ['scrapy_do'],
+    include_package_data = True,
+    package_data={
+        '': ['*.conf'],
+    },
+    classifiers = [
+        'Framework :: Scrapy',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+    ],
+)
