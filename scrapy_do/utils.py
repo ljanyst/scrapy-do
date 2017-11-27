215de648f9f12e1e55b117bbacff605747f5d513
Lukasz Janyst <lukasz@jany.st>
2017-11-27 21:43:08 +0100

    utils: Implement a function for stringifying exceptions

---
+++ scrapy_do/utils.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Author: Lukasz Janyst <lukasz@jany.st>
+# Date:   26.11.2017
+#
+# Licensed under the 3-Clause BSD License, see the LICENSE file for details.
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+def exc_repr(e):
+    """
+    Return a string representation of an exception together with the excepion
+    name.
+    """
+    return "{}: {}".format(type(e).__name__, str(e))
