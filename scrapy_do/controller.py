37d4dfdb370d978223d7eebbfbf2077d98acd40d
Lukasz Janyst <lukasz@jany.st>
2017-12-15 15:20:44 +0100

    controller: Make sure scheduled jobs are created properly on restart

--- scrapy_do/controller.py
+++ scrapy_do/controller.py
@@ -102,8 +102,9 @@ class Controller(Service):
         for job in self.schedule.get_jobs(Status.SCHEDULED):
             self.log.info('Re-scheduling: {}'.format(str(job)))
             sch_job = schedule_job(self.scheduler, job.schedule)
-            sch_job.do(lambda: self.schedule_job(job.project, job.spider, 'now',
-                                                 Actor.SCHEDULER))
+            sch_job.do(lambda job: self.schedule_job(job.project, job.spider,
+                                                     'now', Actor.SCHEDULER),
+                       job)
             self.scheduled_jobs[job.identifier] = sch_job
 
         #-----------------------------------------------------------------------
