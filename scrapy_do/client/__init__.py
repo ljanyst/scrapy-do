9eb4ce94f47893045cef6dd33ae24675b4228942
Lukasz Janyst <lukasz@jany.st>
2018-01-22 19:14:15 +0100

    client: Implement ClientException

---
+++ scrapy_do/client/__init__.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# Author: Lukasz Janyst <lukasz@jany.st>
+# Date:   22.01.2018
+#
+# Licensed under the 3-Clause BSD License, see the LICENSE file for details.
+#-------------------------------------------------------------------------------
+
+
+class ClientException(Exception):
+    pass
