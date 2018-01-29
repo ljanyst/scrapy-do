f3a9ae11b3edb961acc448be5bba841caf7ba27d
Lukasz Janyst <lukasz@jany.st>
2018-01-29 08:18:33 +0100

    webui: Implement Hello World Scrapy Do App

---
+++ ui/src/components/ScrapyDoApp.js
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 29.01.2018
+//
+// Licensed under the 3-Clause BSD License, see the LICENSE file for details.
+//------------------------------------------------------------------------------
+
+import React, { Component } from 'react';
+
+class ScrapyDoApp extends Component {
+  render() {
+    return (
+      <div>
+        Hello, World!
+      </div>
+    );
+  }
+}
+
+export default ScrapyDoApp;
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,16 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 29.01.2018
+//
+// Licensed under the 3-Clause BSD License, see the LICENSE file for details.
+//------------------------------------------------------------------------------
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
+import ScrapyDoApp from './components/ScrapyDoApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ScrapyDoApp />, document.getElementById('root'));
 registerServiceWorker();
