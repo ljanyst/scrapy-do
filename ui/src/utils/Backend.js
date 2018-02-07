82cf5cc38b83e8be98cdd0ace4194745f799e0c0
Lukasz Janyst <lukasz@jany.st>
2018-02-07 19:12:21 +0100

    webui: Implement the reconnect logic in the backend class

--- ui/src/utils/Backend.js
+++ ui/src/utils/Backend.js
@@ -8,13 +8,15 @@
 //------------------------------------------------------------------------------
 // Backend
 //------------------------------------------------------------------------------
-class Backend {
+export class Backend {
   //----------------------------------------------------------------------------
   // Constants
   //----------------------------------------------------------------------------
-  static OPENED = 0;
-  static CLOSED = 1;
+  static CONNECTING = 0;
+  static OPENED = 1;
+  static CLOSED = 2;
   static MSG_RECEIVED = 3;
+  static COUNTDOWN = 4;
 
   //----------------------------------------------------------------------------
   // Constructor
@@ -28,6 +30,9 @@ class Backend {
         : 'ws://localhost:7654/ws';
 
     this.ws = null;
+    this.countdownTimer = null;
+    this.countdown = 0;
+    this.nextTry = 2;
     this.eventListeners = new Set();
     this.connect();
   }
@@ -36,22 +41,67 @@ class Backend {
   // Connect
   //----------------------------------------------------------------------------
   connect = () => {
+    //--------------------------------------------------------------------------
+    // Check and clear the existing state
+    //--------------------------------------------------------------------------
+    if(this.ws)
+      throw Error('Socket not closed');
+
+    if(this.countdownTimer)
+      clearInterval(this.countdownTimer);
+
+    //--------------------------------------------------------------------------
+    // Attempt the connection
+    //--------------------------------------------------------------------------
+    for(const listener of this.eventListeners)
+      listener(Backend.CONNECTING, null);
+
     this.ws = new WebSocket(this.wsUrl);
 
+    //--------------------------------------------------------------------------
+    // On open
+    //--------------------------------------------------------------------------
     this.ws.onopen = () => {
       for(const listener of this.eventListeners)
         listener(Backend.OPENED, null);
+      this.nextTry = 2;
     };
 
+    //--------------------------------------------------------------------------
+    // On Message
+    //--------------------------------------------------------------------------
     this.ws.onmessage = (evt) => {
       const message = JSON.parse(evt.data);
       for(const listener of this.eventListeners)
         listener(Backend.MSG_RECEIVED, message);
     };
 
+    //--------------------------------------------------------------------------
+    // On Close
+    //--------------------------------------------------------------------------
     this.ws.onclose = () => {
       for(const listener of this.eventListeners)
         listener(Backend.CLOSED, null);
+
+      this.ws = null;
+      this.nextTry *= 2;
+      if(this.nextTry > 256)
+        this.nextTry = 256;
+      this.countdown = this.nextTry - 1;
+
+      for(const listener of this.eventListeners)
+        listener(Backend.COUNTDOWN, this.countdown + 1);
+
+      const tick = () => {
+        for(const listener of this.eventListeners)
+          listener(Backend.COUNTDOWN, this.countdown);
+        if(this.countdown === 0) {
+          this.connect();
+          return;
+        }
+        this.countdown -= 1;
+      };
+      this.countdownTimer = setInterval(tick, 1000);
     };
   };
 
