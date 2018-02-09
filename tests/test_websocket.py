80fc4752c350efb05f6514a5e6de5b754b5dee5d
Lukasz Janyst <lukasz@jany.st>
2018-02-09 17:19:51 +0100

    test: Test sending server status over websocket

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -8,18 +8,28 @@
 import unittest
 
 from scrapy_do.websocket import WSFactory, WSProtocol
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 
 #-------------------------------------------------------------------------------
 class WebSocketTests(unittest.TestCase):
 
     #---------------------------------------------------------------------------
-    def test_creation(self):
+    def test_server_status(self):
         controller = Mock()
+        controller.counter_run = 0
+        controller.start_time.timestamp.return_value = 0
+        controller.projects = []
+        controller.scheduled_jobs = []
+        controller.counter_cancel = 0
+        controller.counter_failure = 0
+        controller.counter_success = 0
+        controller.counter_run = 0
         factory = WSFactory(controller=controller)
         factory.protocol = WSProtocol
         protocol = factory.buildProtocol(None)
-        protocol.onOpen()
-        protocol.onMessage(None, None)
-        protocol.onClose(None, None, None)
+
+        with patch.object(WSProtocol, "sendMessage"):
+            protocol.onOpen()
+            protocol.onMessage(None, None)
+            protocol.onClose(None, None, None)
