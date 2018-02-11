273eb3514c7f93ae08cba774497c5f641f687fe2
Lukasz Janyst <lukasz@jany.st>
2018-02-11 21:03:32 +0100

    webui: Implement the daemon status part of the dashboard

--- ui/src/components/Dashboard.js
+++ ui/src/components/Dashboard.js
@@ -6,15 +6,43 @@
 //------------------------------------------------------------------------------
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment-timezone';
 
+//------------------------------------------------------------------------------
+// Dashboard
+//------------------------------------------------------------------------------
 class Dashboard extends Component {
   render() {
+    const serverTime = moment.unix(this.props.time)
+          .tz('Europe/Zurich').format('YYYY-MM-DD HH:mm');
     return(
-      <div>
-        Dashboard
+      <div className='col-md-6 col-md-offset-3'>
+        <div className='dashboard-content'>
+          <img src='scrapy-do-logo.png' alt='Scrapy-Do Logo' />
+          <p>Version {this.props.daemonVersion} @ {this.props.hostname}</p>
+          <p>Server time: {serverTime} ({this.props.timezone})</p>
+          <p>
+            Uptime: {this.props.uptime} | CPU Usage: {this.props.cpuUsage}% |
+            Memory Usage: {this.props.memoryUsage}MB
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-export default Dashboard;
+//------------------------------------------------------------------------------
+// The redux connection
+//------------------------------------------------------------------------------
+function mapStateToProps(state, ownProps) {
+  return {
+    ...state.status
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
