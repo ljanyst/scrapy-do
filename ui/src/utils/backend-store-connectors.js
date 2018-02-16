42d387fc56a486a973e2f40c59361b66652f5b25
Lukasz Janyst <lukasz@jany.st>
2018-02-16 17:47:55 +0100

    webui: Connect project push and remove messages to redux

--- ui/src/utils/backend-store-connectors.js
+++ ui/src/utils/backend-store-connectors.js
@@ -10,7 +10,9 @@ import { Backend } from './Backend';
 import {
   daemonStatusSet, projectsStatusSet, jobsStatusSet
 } from '../actions/status';
-import { projectListSet } from '../actions/projects';
+import {
+  projectListSet, projectPush, projectRemove
+} from '../actions/projects';
 
 //------------------------------------------------------------------------------
 // Make backend events change the state of the stare
@@ -31,6 +33,12 @@ export const messageStoreEvent = (event, data) => {
     break;
   case 'PROJECT_LIST':
     store.dispatch(projectListSet(data.projects));
+    break;
+  case 'PROJECT_PUSH':
+    store.dispatch(projectPush(data.name, data.spiders));
+    break;
+  case 'PROJECT_REMOVE':
+    store.dispatch(projectRemove(data.name));
     break;
 
   default:
