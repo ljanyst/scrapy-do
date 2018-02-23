412a7b552ebc599d27eca72926a7645d976a5692
Lukasz Janyst <lukasz@jany.st>
2018-02-23 20:48:55 +0100

    webui: Finalize the UI of JobListItem

--- ui/src/components/JobListItem.js
+++ ui/src/components/JobListItem.js
@@ -6,7 +6,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { ListGroupItem } from 'react-bootstrap';
+import { ListGroupItem, Button, Glyphicon } from 'react-bootstrap';
+import moment from 'moment-timezone';
+
+import { BACKEND_OPENED } from '../actions/backend';
+import { capitalizeFirst } from '../utils/helpers';
+
+//------------------------------------------------------------------------------
+// Convert status to label class
+//------------------------------------------------------------------------------
+const statusToLabel = (status) => {
+  switch(status) {
+  case 'SCHEDULED': return 'label-primary';
+  case 'PENDING': return 'label-warning';
+  case 'RUNNING': return 'label-success';
+  case 'CANCELED': return 'label-warning';
+  case 'SUCCESSFUL': return 'label-success';
+  case 'FAILED': return 'label-danger';
+  default: return 'label-default';
+  }
+};
+
+//------------------------------------------------------------------------------
+// Get log URL
+//------------------------------------------------------------------------------
+const getLogURL = (jobID, isError) => {
+  const logType = isError ? 'err' : 'out';
+  return `/get-log/data/${jobID}.${logType}`;
+};
 
 //------------------------------------------------------------------------------
 // Job List Item
@@ -25,10 +52,62 @@ class JobListItem extends Component {
   //----------------------------------------------------------------------------
   render() {
     const job = this.props;
+    const timestamp = moment.unix(job.timestamp);
+    const dateTime = timestamp.tz(this.props.timezone)
+          .format('YYYY-MM-DD HH:mm:ss');
+
+    //--------------------------------------------------------------------------
+    // Cancellation button
+    //--------------------------------------------------------------------------
+    let secondaryPanel = (
+      <div className='item-panel' title={dateTime}>
+        <Button
+          bsSize="xsmall"
+          disabled={!this.props.connected}
+        >
+          <Glyphicon glyph='remove-circle'/> Cancel
+        </Button>
+
+      </div>
+    );
+
+    //--------------------------------------------------------------------------
+    // Logs
+    //--------------------------------------------------------------------------
+    if(this.props.jobList === 'COMPLETED') {
+      secondaryPanel = (
+        <div className='item-panel'>
+          {
+            job.outLog
+              ? (<a href={getLogURL(job.identifier, false)}>Out Log</a>)
+              : ''}
+          {job.outLog && job.errLog ? ' ' : ''}
+          {
+            job.errLog
+              ? (<a href={getLogURL(job.identifier, true)}>Error Log</a>)
+              : ''
+          }
+          </div>
+      );
+    }
+
+    //--------------------------------------------------------------------------
+    // Render the whole thing
+    //--------------------------------------------------------------------------
     return (
       <ListGroupItem>
         <div className='list-item'>
-          {job.identifier}
+          <div className='item-panel' title={dateTime}>
+            {timestamp.fromNow()}
+          </div>
+          <span className={`label ${statusToLabel(job.status)}`}>
+            {job.status}
+          </span>
+          <strong> {job.spider}</strong> ({job.project})
+        </div>
+        <div className='list-item-secondary'>
+          {secondaryPanel}
+          Scheduled to run {job.schedule} by {capitalizeFirst(job.actor)}.
         </div>
       </ListGroupItem>
     );
@@ -40,7 +119,9 @@ class JobListItem extends Component {
 //------------------------------------------------------------------------------
 function mapStateToProps(state, ownProps) {
   return {
-    ...state.jobs[ownProps.jobList][ownProps.jobId]
+    ...state.jobs[ownProps.jobList][ownProps.jobId],
+    timezone: state.status.timezone,
+    connected: state.backend.status === BACKEND_OPENED
   };
 }
 
