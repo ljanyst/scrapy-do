f7ddf2d2d016b05f9eb0f4a3942b061ec2d498c3
Lukasz Janyst <lukasz@jany.st>
2018-02-24 11:55:19 +0100

    webui: Implement JOB_REMOVE and JOB_UPDATE actions

--- ui/src/actions/jobs.js
+++ ui/src/actions/jobs.js
@@ -6,11 +6,27 @@
 //------------------------------------------------------------------------------
 
 export const JOB_LIST_SET = 'JOB_LIST_SET';
+export const JOB_UPDATE = 'JOB_UPDATE';
+export const JOB_REMOVE = 'JOB_REMOVE';
 
 export function jobListSet(status, jobs) {
   return {
     type: JOB_LIST_SET,
     status,
     jobs
+  };
+}
+
+export function jobUpdate(job) {
+  return {
+    type: JOB_UPDATE,
+    job
+  };
+}
+
+export function jobRemove(jobId) {
+  return {
+    type: JOB_REMOVE,
+    jobId
   };
 }
--- ui/src/reducers/jobs.js
+++ ui/src/reducers/jobs.js
@@ -5,9 +5,24 @@
 // Licensed under the 3-Clause BSD License, see the LICENSE file for details.
 //------------------------------------------------------------------------------
 
-import { JOB_LIST_SET } from '../actions/jobs';
+import { JOB_LIST_SET, JOB_UPDATE, JOB_REMOVE } from '../actions/jobs';
 
 const jobsState = {};
+
+function filterJob(state, jobId) {
+  let newState = Object.assign(state);
+  if('ACTIVE' in newState)
+    delete newState['ACTIVE'][jobId];
+  if('COMPLETED' in newState)
+    delete newState['COMPLETED'][jobId];
+  return newState;
+}
+
+function statusToListName(status) {
+  if(status === 'SCHEDULED' || status === 'PENDING' || status === 'RUNNING')
+    return 'ACTIVE';
+  return 'COMPLETED';
+}
 
 export function jobsReducer(state = jobsState, action) {
   switch(action.type) {
@@ -21,6 +36,15 @@ export function jobsReducer(state = jobsState, action) {
       ...state,
       [action.status]: jobs
     };
+
+  case JOB_REMOVE:
+    return filterJob(state, action.jobId);
+
+  case JOB_UPDATE:
+    let newState = filterJob(state, action.job.identifier);
+    let listName = statusToListName(action.job.status);
+    newState[listName][action.job.identifier] = action.job;
+    return newState;
 
   default:
     return state;
