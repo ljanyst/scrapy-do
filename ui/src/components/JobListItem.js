5627045c49def2cd96164a38e22342490456032a
Lukasz Janyst <lukasz@jany.st>
2018-02-25 08:36:28 +0100

    webui: Fix the license header

--- ui/src/components/JobListItem.js
+++ ui/src/components/JobListItem.js
@@ -1,6 +1,8 @@
 //------------------------------------------------------------------------------
 // Author: Lukasz Janyst <lukasz@jany.st>
 // Date: 21.02.2018
+//
+// Licensed under the 3-Clause BSD License, see the LICENSE file for details.
 //------------------------------------------------------------------------------
 
 import React, { Component } from 'react';
