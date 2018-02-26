b5e8e767ca002f0f99103909ca63ed3ccf46e4f5
Lukasz Janyst <lukasz@jany.st>
2018-02-26 12:56:13 +0100

    webui: Implement scheduling spec validation

--- ui/src/utils/helpers.js
+++ ui/src/utils/helpers.js
@@ -24,3 +24,88 @@ export function makeId(length) {
 export function capitalizeFirst(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+//------------------------------------------------------------------------------
+// Validate scheduling spec
+//------------------------------------------------------------------------------
+export function scheduleValid(spec) {
+  //----------------------------------------------------------------------------
+  // Now is a valid spec
+  //----------------------------------------------------------------------------
+  if(spec === 'now')
+    return true;
+
+  //----------------------------------------------------------------------------
+  // Check the directives
+  //----------------------------------------------------------------------------
+  let directives = spec.toLowerCase().split(' ');
+  if(directives.length === 0)
+    return false;
+
+  if(directives[0] !== 'every')
+    return false;
+
+  if(directives.length < 2)
+    return false;
+
+  //----------------------------------------------------------------------------
+  // Check the interval
+  //----------------------------------------------------------------------------
+  const interval = parseInt(directives[1], 10);
+  if(!isNaN(interval)) {
+    if(directives.length < 3)
+      return false;
+    directives = directives.slice(2);
+  }
+  else
+    directives = directives.slice(1);
+
+  //----------------------------------------------------------------------------
+  // Check the directives
+  //----------------------------------------------------------------------------
+  const directive_names = [
+    'second', 'seconds', 'minute', 'minutes', 'hour', 'hours', 'day', 'days',
+    'week', 'weeks', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+    'saturday', 'sunday', 'at', 'to'];
+  directives = directives.reverse();
+
+  while(directives.length) {
+    //--------------------------------------------------------------------------
+    // Check the directive
+    //--------------------------------------------------------------------------
+    const directive = directives.pop();
+    if(directive_names.indexOf(directive) < 0)
+      return false;
+
+    //--------------------------------------------------------------------------
+    // Check the argument to "to"
+    //--------------------------------------------------------------------------
+    if(directive === 'to') {
+      if(!directives.length)
+        return false;
+      let arg = directives.pop();
+      arg = parseInt(arg, 10);
+      if(isNaN(arg))
+        return false;
+    }
+    //--------------------------------------------------------------------------
+    // Check the argument to "at"
+    //--------------------------------------------------------------------------
+    else if(directive === 'at') {
+      if(!directives.length)
+        return false;
+      let arg = directives.pop();
+      let argSplit = arg.split(':');
+      if(argSplit.length !== 2)
+        return false;
+      arg = parseInt(argSplit[0], 10);
+      if(isNaN(arg))
+        return false;
+      arg = parseInt(argSplit[1], 10);
+      if(isNaN(arg))
+        return false;
+    }
+  }
+
+  return true;
+}
